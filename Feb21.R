fea7c15b7c98c5b14c3400d449f8280e6df67e27
Ben Michael Beaudin <bbeaudin@uvm.edu>
2019-02-26 08:32:35 -0500

    update code from class

--- Feb21.R
+++ Feb21.R
@@ -14,3 +14,13 @@ BreadMilk
 G <- read.csv('groceriesALL.csv')
 # For next class, try using the same code for rule
 # Butter -> Milk
+ButterMilk <- G %>% 
+  summarise(sumB = sum(butter),
+            nT = n(),
+            supB = sumB / nT,
+            sumBMi = sum(butter * whole.milk),
+            supBMi = sumBMi / nT,
+            confBMi = supBMi / supB,
+            sumMi = sum(whole.milk),
+            supMi = sumMi / nT,
+            liftBMi = confBMi / supMi)
